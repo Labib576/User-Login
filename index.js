f2be95e66c21468984e00c7b905d633607a30307
Labib <labibsharia@gmail.com>
2025-11-10 16:23:57 +0600

    change

--- index.js
+++ index.js
@@ -4,4 +4,36 @@ const num1 = 11.5421
 console.log(parseInt(num1));
 console.log(parseFloat(num1));
 
-let name = "labib"
+if( 1>2) {
+    console.log("2 boro");
+}
+if (1==1) {
+    console.log("ei duita soman hobe");
+}
+ else if(1>2) {
+    console.log('1 boro');
+}
+else {
+    console.log("2 boro na");
+}
+
+let age = 23;
+if (age>50) {
+    console.log("tumi akta bura");
+}
+else if (age<20) {
+    console.log("tumi akta baccha");
+}
+else {
+    console.log("kono tai na");
+}
+
+
+let person = {
+    name: "Maeen Sharia",
+    age: 23,
+    status: "Single"
+
+};
+ console.log(person);
+ 
